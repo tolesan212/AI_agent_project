eb788e9cf5e66268f0745d31338cfe0f3abed9e2
tolesan212 <tolesan212@gmail.com>
2025-12-14 15:54:56 -0500

    verboseflag, calculator files, and get files function

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+
+#- README.md: file_size=1032 bytes, is_dir=False
+#- src: file_size=128 bytes, is_dir=True
+#- package.json: file_size=1234 bytes, is_dir=False
+
+
+def get_files_info(working_directory, directory="."):
+    abs_working_dir = os.path.abspath(working_directory) #absolute path of working directory
+    abs_directory = os.path.abspath(directory) #absolute path of specified directory
+
+    #If the absolute directory is not in the working directory, its outside of it, and we return an error message
+    if not abs_directory.startswith(abs_working_dir):
+        return f'Error: The specified directory "{directory}" is not within the working directory.'
+    
+    final_response = ""
+    contents = os.listdir(abs_directory) #list of all files and directories in the specified directory
+    # for every content in the directory
+    for content in contents:
+        content_path = os.path.join(abs_directory, content) #absolute path of the content
+        is_dir = os.path.isdir(content_path) #check if the content is a directory
+        size = os.path.getsize(content_path) #get the size of the content in bytes
+        final_response += f"- {content}: file_size={size} bytes, is_dir={is_dir}\n" #add the information to the final response string
+    return final_response
+
--- main.py
+++ main.py
@@ -16,9 +16,21 @@ def main():
         print("I need a prompt to generate a response.")
         sys.exit(1) # Exit with error code 1 if no prompt is provided
     
+    verbose_flag = False
+    # Check if --verbose flag is provided as command line argument
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag = True
+
+
+
     # sys.argv[0] = name of file
     # sys.argv[1] = prompt, first command line argument
     prompt = sys.argv[1]
+
+    #Create a list of messages, starting with the user's prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+    #Creating a list of messages helps give context to Gemini about the conversation history.
+
 
     # Generate response from Gemini
     response = client.models.generate_content(
@@ -33,9 +45,11 @@ def main():
         print("response is malformed")
         return
 
-    # Print usage metadata
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # If --verbose flag is provided, print usage metadata
+    if verbose_flag:
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 main()
