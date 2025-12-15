a78b34a65e028425f2a245441538a8a81ad24b67
tolesan212 <tolesan212@gmail.com>
2025-12-14 21:45:15 -0500

    run python file function added

---
+++ functions/run_python_file.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+
+def run_python_file(working_directory: str, file_path: str):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    #check if the file exists, is a python file, and its path is within the working directory
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: File path "{file_path}" is outside the working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: file "{file_path}" is not a file'
+    if not file_path.endswith('.py'):
+        return f'Error: file "{file_path}" is not a Python file'
+    
+    #run the Python file using subprocess.run
+    try:
+        output = subprocess.run(
+            ["python3", file_path], 
+            cwd=abs_working_dir,
+            timeout=30,
+            capture_output=True
+            )
+        
+        final_string = f"STDOUT: {output.stdout}\nSTDERR: {output.stderr}"
+        
+        if output.stdout == "" and output. stderr == "":
+            final_string = "The Python file did not produce any output.\n"
+        if output.returncode != 0:
+            final_string = f"Process exited with code {output.returncode}.\n"
+        return final_string
+
+    except Exception as e:
+        return f'Error: executing Python file {e}'
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
     working_dir = "calculator"
@@ -28,6 +29,10 @@ def main():
     #print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum!")) #creates a new file if it doesn't exist
     #print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed")) #/tmp is outside of working directory (root of file system)
     
+    #test run_python_file
+    #print(run_python_file(working_dir, "main.py"))
+    print(run_python_file(working_dir, "tests.py"))
+
 
 
 main()
