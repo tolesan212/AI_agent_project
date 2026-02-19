99ca1b0118bb27abdd5500fc2a12024ccd140cd2
tolesan212 <tolesan212@gmail.com>
2026-02-19 11:56:19 -0500

    tmp

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,5 +1,6 @@
 import os
 
+
 def write_file(working_directory, file_path, content):
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
